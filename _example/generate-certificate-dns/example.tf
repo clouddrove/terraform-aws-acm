4b0bd5d6651d02cbe876216ac438a728a96be5e9
Kamlesh <kamlesh@clouddrove.com>
2019-08-28 16:07:27 +0530

    update readme

--- _example/generate-certificate-dns/example.tf
+++ _example/generate-certificate-dns/example.tf
@@ -4,14 +4,14 @@ provider "aws" {
 
 module "acm" {
   source = "git::https://github.com/clouddrove/terraform-aws-acm.git"
-  
+
   name        = "certificate"
   application = "clouddrove"
   environment = "test"
   label_order = ["environment", "name", "application"]
 
-  domain_name       = "clouddrove.com"
-  validation_method = "DNS"
-  enable_dns_validation    = false
+  domain_name           = "clouddrove.com"
+  validation_method     = "DNS"
+  enable_dns_validation = false
 }
 
