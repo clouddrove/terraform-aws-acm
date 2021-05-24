9777f6589457779c74d476158c7e9142529b583a
yadavprakash <yadavprakash.it@gmail.com>
2021-05-24 17:54:25 +0530

    add-multipal-doamin

--- _example/generate-certificate-dns/example.tf
+++ _example/generate-certificate-dns/example.tf
@@ -9,8 +9,9 @@ module "acm" {
   environment = "test"
   label_order = ["name", "environment"]
 
-  enable_aws_certificate = true
-  domain_name            = "clouddrove.com"
-  validation_method      = "DNS"
-  enable_dns_validation  = false
+  enable_aws_certificate    = true
+  domain_name               = "clouddrove.com"
+  subject_alternative_names = ["www.clouddrove.com"]
+  validation_method         = "DNS"
+  enable_dns_validation     = false
 }
